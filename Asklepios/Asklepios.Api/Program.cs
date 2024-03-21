0ac1b02f8f594bccdc1a8bbd37efd8b0e4e63166
Kamil Matusz <matuszkmail076@gmail.com>
2024-03-21 19:54:58 +0100

    Upload custom extensions

--- Asklepios/Asklepios.Api/Program.cs
+++ Asklepios/Asklepios.Api/Program.cs
@@ -1,4 +1,16 @@
+using Asklepios.Application;
+using Asklepios.Core;
+using Asklepios.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddCore()
+    .AddApplication()
+    .AddInfrastructure();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
