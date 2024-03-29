883fe3f6507e1a649ba66a44a42b075529d47049
Kamil Matusz <matuszkmail076@gmail.com>
2024-03-29 21:30:13 +0100

    CORS & Swagger

--- Asklepios/Asklepios.Api/Program.cs
+++ Asklepios/Asklepios.Api/Program.cs
@@ -15,6 +15,8 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.UseInfrastructure();
+
 app.MapControllers();
 
 app.Run();
--- Asklepios/Asklepios.Infrastructure/Extensions.cs
+++ Asklepios/Asklepios.Infrastructure/Extensions.cs
@@ -1,12 +1,17 @@
 using Asklepios.Infrastructure.DAL;
 using Asklepios.Infrastructure.Repositories.Departments;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace Asklepios.Infrastructure;
 
 public static class Extensions
 {
+    private const string CorsPolicy = "cors";
+    
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddPostgres(configuration);
@@ -14,7 +19,44 @@ public static class Extensions
         services.AddSingleton<IDepartmentRepository, InMemoryDepartmentRepository>();
         services.AddSingleton<IRoomRepository, InMemoryRoomRepository>();
         
+        // CORS
+        services.AddCors(cors =>
+        {
+            cors.AddPolicy(CorsPolicy, x =>
+            {
+                x.WithOrigins("*")
+                    .WithMethods("POST", "PUT", "DELETE")
+                    .WithHeaders("Content-Type", "Authorization");
+            });
+        });
+        
+        // Swagger Documentation
+        services.AddSwaggerGen(swagger =>
+        {
+            swagger.CustomSchemaIds(x => x.FullName);
+            swagger.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Asklepios - Hospital Managment System",
+                Version = "v1"
+            });
+        });
+        
         return services;
+    }
+    
+    public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseCors(CorsPolicy);
+
+        app.UseSwagger();
+        app.UseSwaggerUI(swagger =>
+        {
+            swagger.RoutePrefix = "swagger";
+            swagger.DocumentTitle = "Asklepios - Hospital Managment System";
+        });
+        
+        app.UseRouting();
+        return app;
     }
     
     public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : class, new()
