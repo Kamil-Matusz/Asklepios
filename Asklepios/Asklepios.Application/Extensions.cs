02eb3807e5d92969ab55b4c2cc510b278c6194d7
Awahir <matuszkamil076@gmail.com>
2024-04-07 21:41:09 +0200

    Created CQRS skeleton

--- Asklepios/Asklepios.Application/Extensions.cs
+++ Asklepios/Asklepios.Application/Extensions.cs
@@ -1,3 +1,4 @@
+using Asklepios.Application.Abstractions;
 using Asklepios.Application.Services.Departments;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,12 @@ public static class Extensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        var applicationAssembly = typeof(ICommandHandler<>).Assembly;
+        services.Scan(s => s.FromAssemblies(applicationAssembly)
+            .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+        
         services.AddScoped<IDeparmentService, DepartmentService>();
         services.AddScoped<IRoomService, RoomService>();
         
