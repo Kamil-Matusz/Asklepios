229ac7edfbead19e8a6c44c5af644bae7d5aefcf
Awahir <matuszkamil076@gmail.com>
2024-05-01 20:11:43 +0200

    Created patients entity & dto

---
+++ Asklepios/Asklepios.Core/Validators/Extensions.cs
@@ -0,0 +1,23 @@
+using Asklepios.Core.Validators.Departments;
+using Asklepios.Core.Validators.Patients;
+using Asklepios.Core.Validators.Users;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Asklepios.Core.Validators;
+
+public static class Extensions
+{
+    public static IServiceCollection AddFluentValidator(this IServiceCollection services)
+    {
+        services.AddFluentValidation(fv => fv
+            .RegisterValidatorsFromAssemblyContaining<DepartmentDtoValidator>()
+            .RegisterValidatorsFromAssemblyContaining<RoomDtoValidator>()
+            .RegisterValidatorsFromAssemblyContaining<UserDtoValidator>()
+            .RegisterValidatorsFromAssemblyContaining<NurseDtoValidator>()
+            .RegisterValidatorsFromAssemblyContaining<MedicalStaffDtoValidator>()
+            .RegisterValidatorsFromAssemblyContaining<PatientDtoValidator>());
+
+        return services;
+    }
+}
