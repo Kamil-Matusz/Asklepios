11775fdedfa90a0ec3145a4f7222752b4baf253d
Awahir <matuszkamil076@gmail.com>
2024-07-03 21:46:20 +0200

    Added components

---
+++ Asklepios.Web/src/components/pages/BasePage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import BaseCardWithHover from '../cards/BaseCardWithHover.vue';
+
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="ml-14 h-screen pa-8 pb-12 d-flex flex-column">
+    <h2 class="text-h4 text-white my-4 text-left">{{ title }}</h2>
+    <slot name="above-card"></slot>
+        <slot></slot>
+  </div>
+</template>
+
+<style>
+.hide-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+</style>
