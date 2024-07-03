04aa15f9f7f48a41d71ea763a17aec2a12d590b9
Awahir <matuszkamil076@gmail.com>
2024-07-03 21:10:22 +0200

    Configured jwt elements

--- Asklepios.Web/src/services/httpClient.ts
+++ Asklepios.Web/src/services/httpClient.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
+import { addErrorInterceptor } from './interceptors/errorInterceptors';
+import { addJwtInterceptor } from './interceptors/jwtInterceptor';
 
 const httpClient = axios.create({
   baseURL: "http://localhost:5102",
 });
 
+addErrorInterceptor(httpClient);
+addJwtInterceptor(httpClient);
 
 export default httpClient
