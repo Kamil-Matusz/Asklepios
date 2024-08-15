d57bf369a4edad34cb24b056ab66ab29c561037d
Awahir <matuszkamil076@gmail.com>
2024-08-15 16:54:40 +0200

    404 view configuration

--- Asklepios.Web/src/router/index.ts
+++ Asklepios.Web/src/router/index.ts
@@ -30,6 +30,7 @@ import DischargePatientView from '@/views/DischargePatientView.vue';
 import DoctorDischargesView from '@/views/DoctorDischargesView.vue';
 import DischargesView from '@/views/DischargesView.vue';
 import DashboardView from '@/views/DashboardView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -158,6 +159,11 @@ const router = createRouter({
       name: 'Dashboard',
       component: DashboardView
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFoundView
+    }
   ]
 })
 
