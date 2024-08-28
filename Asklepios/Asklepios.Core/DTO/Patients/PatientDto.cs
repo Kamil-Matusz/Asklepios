d36db0cb55a6fb66d7db75e5009ccde122f22d58
Awahir <matuszkamil076@gmail.com>
2024-08-28 21:14:33 +0200

    New columns on the Patients table

--- Asklepios/Asklepios.Core/DTO/Patients/PatientDto.cs
+++ Asklepios/Asklepios.Core/DTO/Patients/PatientDto.cs
@@ -12,4 +12,6 @@ public class PatientDto
     public Guid DepartmentId { get; set; }
     public Guid RoomId { get; set; }
     public Guid MedicalStaffId { get; set; }
+    public DateOnly AdmissionDate { get; set; }
+    public string Address { get; set; }
 }
--- Asklepios/Asklepios.Infrastructure/DAL/Seeders/Patients/PatientsSeeder.cs
+++ Asklepios/Asklepios.Infrastructure/DAL/Seeders/Patients/PatientsSeeder.cs
@@ -13,8 +13,8 @@ public class PatientsSeeder : IOrderedSeeder
         {
             var newPatients = new List<Patient>()
             {
-                new Patient(Guid.Parse("bc06de0b-ca04-4341-a2df-492bc4f6cecf"), "Konrad", "Bieniasz", "01300406346", "Zawał serca", false, "Operacja", Guid.Parse("2a9f1b22-1854-43f2-a30b-c34d3d70f2c9"), Guid.Parse("c1058167-d03e-4086-ac83-7c6f183a010d"), Guid.Parse("e582299d-1a49-4d7b-8e36-eadb449dd209")),
-                new Patient(Guid.Parse("bc06de0b-ca04-4341-a2df-492bc4f6cebf"), "Michał", "Jaros", "01300405346", "Wszczepie bajpasów", false, "Operacja", Guid.Parse("2a9f1b22-1854-43f2-a30b-c34d3d70f2c9"), Guid.Parse("c1058167-d03e-4086-ac83-7c6f183a010d"), Guid.Parse("e582299d-1a49-4d7b-8e36-eadb449dd209"))
+                new Patient(Guid.Parse("bc06de0b-ca04-4341-a2df-492bc4f6cecf"), "Konrad", "Bieniasz", "01300406346", "Zawał serca", false, "Operacja", Guid.Parse("2a9f1b22-1854-43f2-a30b-c34d3d70f2c9"), Guid.Parse("c1058167-d03e-4086-ac83-7c6f183a010d"), Guid.Parse("e582299d-1a49-4d7b-8e36-eadb449dd209"), DateOnly.FromDateTime(DateTime.Today), "Akacjowa 24 35-211 Rzeszów"),
+                new Patient(Guid.Parse("bc06de0b-ca04-4341-a2df-492bc4f6cebf"), "Michał", "Jaros", "01300405346", "Wszczepie bajpasów", false, "Operacja", Guid.Parse("2a9f1b22-1854-43f2-a30b-c34d3d70f2c9"), Guid.Parse("c1058167-d03e-4086-ac83-7c6f183a010d"), Guid.Parse("e582299d-1a49-4d7b-8e36-eadb449dd209"), DateOnly.FromDateTime(DateTime.Today), "Wojska Polskiego 12 36-040 Boguchwała")
             };
             
             dbContext.Patients.AddRange(newPatients);
