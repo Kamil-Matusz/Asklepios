1ddf2056aadd1780c0fff75f00f8346a0cd77367
Awahir <matuszkamil076@gmail.com>
2024-09-28 20:17:35 +0200

    Add tests for the SignalR

--- Asklepios/Asklepios.Infrastructure/Events/Extensions.cs
+++ Asklepios/Asklepios.Infrastructure/Events/Extensions.cs
@@ -9,9 +9,6 @@ public static class Extensions
 {
     public static IServiceCollection AddEvents(this IServiceCollection services)
     {
-        /*services.AddTransient<IEventHandler<DischargePatient>, DischargePersonEventHandler>();
-        services.AddTransient<IEventHandler<UpdateDischargeStatus>, UpdateDischargeStatusHandler>();*/
-        
         var infrastructureAssembly = typeof(AppOptions).Assembly;
         services.Scan(s => s.FromAssemblies(infrastructureAssembly)
             .AddClasses(c => c.AssignableTo(typeof(IEventHandler<>)))
