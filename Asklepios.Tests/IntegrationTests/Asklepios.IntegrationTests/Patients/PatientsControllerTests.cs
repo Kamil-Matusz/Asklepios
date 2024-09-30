c505f9ae1df1536a29c886f0dff8f59b3c5761ba
Awahir <matuszkamil076@gmail.com>
2024-09-30 20:57:30 +0200

    Updated patients controller tests

--- Asklepios.Tests/IntegrationTests/Asklepios.IntegrationTests/Patients/PatientsControllerTests.cs
+++ Asklepios.Tests/IntegrationTests/Asklepios.IntegrationTests/Patients/PatientsControllerTests.cs
@@ -5,8 +5,6 @@ using Asklepios.Core.Entities.Departments;
 using Asklepios.Core.Entities.Patients;
 using Asklepios.Core.Entities.Users;
 using Asklepios.Core.ValueObjects;
-using Asklepios.Infrastructure.Security;
-using Microsoft.AspNetCore.Identity;
 using Shouldly;
 
 namespace Asklepios.IntegrationTests.Patients;
@@ -27,6 +25,23 @@ public class PatientsControllerTests : BaseControllerTest, IDisposable
     }
     
     [Fact]
+    public async Task GetAllPatients_ShouldReturn_Ok_WhenUserIsInGoodRole()
+    {
+        // Arrange
+        var admin = new User(Guid.NewGuid(), "admin@test.com", "password", Role.Admin(), true, DateTime.Now);
+        Authorize(admin.UserId, admin.Role);
+
+        const int pageIndex = 1;
+        const int pageSize = 10;
+
+        // Act
+        var response = await Client.GetAsync($"/patients-module/Patients?pageIndex={pageIndex}&pageSize={pageSize}");
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+    
+    [Fact]
     public async Task GetPatientById_ShouldReturnUnauthorized_WhenUserIsNotAuthorized()
     {
         // Arrange
@@ -40,133 +55,132 @@ public class PatientsControllerTests : BaseControllerTest, IDisposable
         response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
     }
     
-    /*[Fact]
-    public async Task Create_Patient_Should_Return_Ok_Status()
+    [Fact]
+    public async Task CreatePatient_ShouldReturn_Created_WhenValidDataIsProvided()
     {
         // Arrange
-        var passwordManager = new PasswordManager(new PasswordHasher<User>());
-        const string password = "password";
-
-        var doctor = new User(Guid.NewGuid(), "patientDoctor1@test.com", passwordManager.Secure(password), Role.Doctor(), true,
-            DateTime.Now);
-
-        var patientDto = new PatientDto
+        var admin = new User(Guid.NewGuid(), "admin@test.com", "password", Role.Admin(), true, DateTime.Now);
+        var department = new Department { DepartmentId = Guid.NewGuid(), DepartmentName = "Neurologia", NumberOfBeds = 50, ActualNumberOfPatients = 20};
+        var room = new Room { RoomId = Guid.NewGuid(), RoomNumber = 105, RoomType = "Ogólna", NumberOfBeds = 4, DepartmentId = department.DepartmentId};
+        var user = new User(Guid.NewGuid(), "user1@test.com", "password", Role.Doctor(), true, DateTime.Now);
+        var doctor = new MedicalStaff { DoctorId = Guid.NewGuid(), Name = "Test", Surname = "Test", PrivatePhoneNumber = "+48123456789", HospitalPhoneNumber = "+48987654321", Specialization = "Test", MedicalLicenseNumber = "ABC123", UserId = user.UserId, DepartmentId = department.DepartmentId };
+        await _testDatabase.DbContext.Users.AddAsync(admin);
+        await _testDatabase.DbContext.Departments.AddAsync(department);
+        await _testDatabase.DbContext.Rooms.AddAsync(room);
+        await _testDatabase.DbContext.Users.AddAsync(user);
+        await _testDatabase.DbContext.MedicalStaff.AddAsync(doctor);
+        await _testDatabase.DbContext.SaveChangesAsync();
+        
+        var newPatient = new PatientDto
         {
             PatientId = Guid.NewGuid(),
             PatientName = "Test",
             PatientSurname = "Test",
-            PeselNumber = "01300406342",
+            PeselNumber = "01300407251",
             InitialDiagnosis = "Test",
             IsDischarged = false,
-            Treatment = "test",
-            DepartmentId = Guid.NewGuid(),
-            RoomId = Guid.NewGuid(),
-            MedicalStaffId = Guid.NewGuid(),
-            AdmissionDate = DateOnly.FromDateTime(DateTime.Today),
-            Address = "Test Address"
-        };
-
-        await _testDatabase.DbContext.Users.AddAsync(doctor);
-        await _testDatabase.DbContext.SaveChangesAsync();
-
-        // Act
-        Authorize(doctor.UserId, doctor.Role);
-        var response = await Client.PostAsJsonAsync("patients-module/Patients", patientDto);
-        
-        // Assert
-        response.StatusCode.ShouldBe(HttpStatusCode.OK);
-    }
-    
-    [Fact]
-    public async Task GetAllPatients_ShouldReturn_Ok_Status_And_Patients()
-    {
-        // Arrange
-        const int pageIndex = 1;
-        const int pageSize = 10;
-
-        var department = new Department { DepartmentId = Guid.NewGuid(), DepartmentName = "Department 1", NumberOfBeds = 50, ActualNumberOfPatients = 20 };
-        var room = new Room {RoomId = Guid.NewGuid(), RoomNumber = 115, RoomType = "Normal", NumberOfBeds = 4, DepartmentId = department.DepartmentId};
-            
-        var patients = new List<Patient>
-        {
-            new Patient {
-                PatientId = Guid.NewGuid(),
-                PatientName = "Test",
-                PatientSurname = "Test",
-                PeselNumber = "01300406342",
-                InitialDiagnosis = "Test",
-                IsDischarged = false,
-                Treatment = "test",
-                DepartmentId = department.DepartmentId,
-                RoomId = room.RoomId,
-                MedicalStaffId = Guid.NewGuid(),
-                AdmissionDate = DateOnly.FromDateTime(DateTime.Today),
-                Address = "Test Address",
-            }
-        };
-
-        await _testDatabase.DbContext.Departments.AddAsync(department);
-        await _testDatabase.DbContext.Rooms.AddAsync(room);
-        await _testDatabase.DbContext.Patients.AddRangeAsync(patients);
-        await _testDatabase.DbContext.SaveChangesAsync();
-
-        var nurse = new User(Guid.NewGuid(), "katarzynawacnik@test.com", "password", Role.Nurse(), true, DateTime.Now);
-        await _testDatabase.DbContext.Users.AddAsync(nurse);
-        await _testDatabase.DbContext.SaveChangesAsync();
-
-        // Act
-        Authorize(nurse.UserId, nurse.Role);
-    
-        var response = await Client.GetAsync($"/patients-module/Patients?pageIndex={pageIndex}&pageSize={pageSize}");
-        response.EnsureSuccessStatusCode();
-
-        var departmentDtos = await response.Content.ReadFromJsonAsync<IReadOnlyList<PatientListDto>>();
-
-        // Assert
-        departmentDtos.ShouldNotBeNull();
-        departmentDtos.Count.ShouldBe(patients.Count);
-    }
-    
-    [Fact]
-    public async Task DeletePatient_ShouldReturn_NoContent()
-    {
-        var department = new Department { DepartmentId = Guid.NewGuid(), DepartmentName = "Department 1", NumberOfBeds = 50, ActualNumberOfPatients = 20 };
-        var room = new Room {RoomId = Guid.NewGuid(), RoomNumber = 115, RoomType = "Normal", NumberOfBeds = 4, DepartmentId = department.DepartmentId};
-        
-        var patientId = Guid.NewGuid();
-        
-        var patient = new Patient
-        {
-            PatientId = patientId,
-            PatientName = "Test",
-            PatientSurname = "Test",
-            PeselNumber = "01300406242",
-            InitialDiagnosis = "Test",
-            IsDischarged = true,
-            Treatment = "test",
+            Treatment = "Test",
             DepartmentId = department.DepartmentId,
             RoomId = room.RoomId,
+            MedicalStaffId = doctor.DoctorId,
+            AdmissionDate = DateOnly.FromDateTime(DateTime.Today),
+            Address = "Test"
         };
-        
-        await _testDatabase.DbContext.Departments.AddAsync(department);
-        await _testDatabase.DbContext.Rooms.AddAsync(room);
-        await _testDatabase.DbContext.Patients.AddAsync(patient);
-        await _testDatabase.DbContext.SaveChangesAsync();
-        
-        var nurse = new User(Guid.NewGuid(), "nurse@test.com", "password", Role.Nurse(), true, DateTime.Now);
-        await _testDatabase.DbContext.Users.AddAsync(nurse);
-        await _testDatabase.DbContext.SaveChangesAsync();
-        
-        Authorize(nurse.UserId, nurse.Role);
 
         // Act
-        var response = await Client.DeleteAsync($"/patients-module/Patients/{patientId}");
+        Authorize(admin.UserId, admin.Role);
+        var response = await Client.PostAsJsonAsync($"/patients-module/Patients", newPatient);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+    }
+    
+    [Fact]
+    public async Task GetPatientsList_ShouldReturn_Ok_WhenCalledByAuthorizedUser()
+    {
+        // Arrange
+        var admin = new User(Guid.NewGuid(), "admin@test.com", "password", Role.Admin(), true, DateTime.Now);
+        await _testDatabase.DbContext.Users.AddAsync(admin);
+        await _testDatabase.DbContext.SaveChangesAsync();
+        
+        // Act
+        Authorize(admin.UserId, admin.Role);
+        var response = await Client.GetAsync($"/patients-module/Patients/patientsList");
+        var patientsList = await response.Content.ReadFromJsonAsync<IEnumerable<PatientAutocompleteDto>>();
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        patientsList.ShouldNotBeNull();
+    }
+    
+    [Fact]
+    public async Task DeletePatient_ShouldReturn_NoContent_WhenPatientIsDeleted()
+    {
+        // Arrange
+        var admin = new User(Guid.NewGuid(), "admin@test.com", "password", Role.Admin(), true, DateTime.Now);
+        var department = new Department { DepartmentId = Guid.NewGuid(), DepartmentName = "Neurologia", NumberOfBeds = 50, ActualNumberOfPatients = 20};
+        var room = new Room { RoomId = Guid.NewGuid(), RoomNumber = 105, RoomType = "Ogólna", NumberOfBeds = 4, DepartmentId = department.DepartmentId};
+        var user = new User(Guid.NewGuid(), "user1@test.com", "password", Role.Doctor(), true, DateTime.Now);
+        var doctor = new MedicalStaff { DoctorId = Guid.NewGuid(), Name = "Test", Surname = "Test", PrivatePhoneNumber = "+48123456789", HospitalPhoneNumber = "+48987654321", Specialization = "Test", MedicalLicenseNumber = "ABC123", UserId = user.UserId, DepartmentId = department.DepartmentId };
+        var patient = new Patient { PatientId = Guid.NewGuid(), PatientName = "Test", PatientSurname = "Test", PeselNumber = "01300407251", InitialDiagnosis = "Test", IsDischarged = false, Treatment = "Test", DepartmentId = department.DepartmentId, RoomId = room.RoomId, MedicalStaffId = doctor.DoctorId, AdmissionDate = DateOnly.FromDateTime(DateTime.Today), Address = "Test" };
+        await _testDatabase.DbContext.Users.AddAsync(admin);
+        await _testDatabase.DbContext.Departments.AddAsync(department);
+        await _testDatabase.DbContext.Rooms.AddAsync(room);
+        await _testDatabase.DbContext.Users.AddAsync(user);
+        await _testDatabase.DbContext.MedicalStaff.AddAsync(doctor);
+        await _testDatabase.DbContext.Patients.AddAsync(patient);
+        await _testDatabase.DbContext.SaveChangesAsync();
+
+        // Act
+        Authorize(admin.UserId, admin.Role);
+        var response = await Client.DeleteAsync($"/patients-module/Patients/{patient.PatientId}");
 
         // Assert
         response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
-        
-        var deletedPatient = await _testDatabase.DbContext.Patients.FindAsync(patientId);
-        deletedPatient.ShouldBeNull();
     }
-    */
+    
+    [Fact]
+    public async Task UpdatePatient_ShouldReturn_NoContent_WhenUpdateIsSuccessful()
+    {
+        // Arrange
+        var admin = new User(Guid.NewGuid(), "admin@test.com", "password", Role.Admin(), true, DateTime.Now);
+        var department = new Department { DepartmentId = Guid.NewGuid(), DepartmentName = "Neurologia", NumberOfBeds = 50, ActualNumberOfPatients = 20};
+        var room = new Room { RoomId = Guid.NewGuid(), RoomNumber = 105, RoomType = "Ogólna", NumberOfBeds = 4, DepartmentId = department.DepartmentId};
+        var user = new User(Guid.NewGuid(), "user1@test.com", "password", Role.Doctor(), true, DateTime.Now);
+        var doctor = new MedicalStaff { DoctorId = Guid.NewGuid(), Name = "Test", Surname = "Test", PrivatePhoneNumber = "+48123456789", HospitalPhoneNumber = "+48987654321", Specialization = "Test", MedicalLicenseNumber = "ABC123", UserId = user.UserId, DepartmentId = department.DepartmentId };
+        var patient = new Patient { PatientId = Guid.NewGuid(), PatientName = "Test", PatientSurname = "Test", PeselNumber = "01300407251", InitialDiagnosis = "Test", IsDischarged = false, Treatment = "Test", DepartmentId = department.DepartmentId, RoomId = room.RoomId, MedicalStaffId = doctor.DoctorId, AdmissionDate = DateOnly.FromDateTime(DateTime.Today), Address = "Test" };
+        await _testDatabase.DbContext.Users.AddAsync(admin);
+        await _testDatabase.DbContext.Departments.AddAsync(department);
+        await _testDatabase.DbContext.Rooms.AddAsync(room);
+        await _testDatabase.DbContext.Users.AddAsync(user);
+        await _testDatabase.DbContext.MedicalStaff.AddAsync(doctor);
+        await _testDatabase.DbContext.Patients.AddAsync(patient);
+        await _testDatabase.DbContext.SaveChangesAsync();
+        
+        var updatePatient = new PatientDto
+        {
+            PatientId = Guid.NewGuid(),
+            PatientName = "Test",
+            PatientSurname = "Test",
+            PeselNumber = "01300407251",
+            InitialDiagnosis = "Test",
+            IsDischarged = false,
+            Treatment = "Test",
+            DepartmentId = department.DepartmentId,
+            RoomId = room.RoomId,
+            MedicalStaffId = doctor.DoctorId,
+            AdmissionDate = DateOnly.FromDateTime(DateTime.Today),
+            Address = "Test"
+        };
+
+        // Act
+        Authorize(admin.UserId, admin.Role);
+        var response = await Client.PutAsJsonAsync($"/patients-module/Patients/{patient.PatientId}", updatePatient);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+        // Verify Update
+       
+    }
 }
