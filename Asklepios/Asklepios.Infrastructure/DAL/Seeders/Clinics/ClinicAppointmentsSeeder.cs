14e38700e5c56d897f2efec1e65d484ac9b29f6e
Awahir <matuszkamil076@gmail.com>
2024-12-11 21:10:41 +0100

    Update clinic module seeders

--- Asklepios/Asklepios.Infrastructure/DAL/Seeders/Clinics/ClinicAppointmentsSeeder.cs
+++ Asklepios/Asklepios.Infrastructure/DAL/Seeders/Clinics/ClinicAppointmentsSeeder.cs
@@ -21,6 +21,22 @@ public class ClinicAppointmentsSeeder : IOrderedSeeder
                     Guid.Parse("db85f588-2250-4479-b826-7c210bbaafac"),
                     Guid.Parse("e582299d-1a49-4d7b-8e36-eadb449dd209"),
                     "Scheduled"
+                ),
+                new ClinicAppointment(
+                    Guid.NewGuid(),
+                    new DateTime(2024, 01, 14, 10, 0, 0),
+                    AppointmentType.Consultation(),
+                    Guid.Parse("db85f588-2250-4479-b826-7c210bbaafac"),
+                    Guid.Parse("e582299d-1a49-4d7b-8e36-eadb449dd209"),
+                    "Scheduled"
+                ),
+                new ClinicAppointment(
+                    Guid.NewGuid(),
+                    new DateTime(2024, 02, 12, 12, 0, 0),
+                    AppointmentType.Consultation(),
+                    Guid.Parse("db84f587-2250-4479-b826-7c210bbaafac"),
+                    Guid.Parse("e582299d-1a49-4d7b-8e36-eadb449dd209"),
+                    "Scheduled"
                 )
             };
         }
