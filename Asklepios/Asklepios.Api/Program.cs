eeae74499acfefccceded05ea2e1490be68bea31
Awahir <matuszkamil076@gmail.com>
2026-01-04 19:35:56 +0100

    Update config for dockerize

--- Asklepios/Asklepios.Api/Program.cs
+++ Asklepios/Asklepios.Api/Program.cs
@@ -1,11 +1,8 @@
 using Asklepios.Application;
 using Asklepios.Core;
 using Asklepios.Infrastructure;
-using Asklepios.Infrastructure.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
-
-//builder.Configuration.AddJsonFile("Properties/secret.json", optional: true, reloadOnChange: true);
 
 QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
 
